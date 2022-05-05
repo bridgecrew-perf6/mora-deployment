87d419f64e06b15b365a56379bdaf17e1f2a7b42
massimo-gollo <massimo.gollo@hotmail.it>
2022-05-05 22:41:52 +0200

    sync

--- helpers/scripts/collect_metrics.sh
+++ helpers/scripts/collect_metrics.sh
@@ -4,4 +4,4 @@ set -exu
 #create snapshot
 curl -XPOST http://$(minikube ip):30003/api/v1/admin/tsdb/snapshot
 
-kubectl cp monitoring/prometheus-kps-"$1"-kube-prometheus-prometheus-0:/prometheus/snapshots ./prom-data/$(date +%s) /
+kubectl cp monitoring/prometheus-kps-"$1"-kube-prometheus-prometheus-0:/prometheus/snapshots ./prom-data/$(date +%s)
