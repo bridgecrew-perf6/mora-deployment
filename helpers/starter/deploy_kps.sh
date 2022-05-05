2fa2eae882bb1c9d8a29ba4bd9e01c582c323488
massimo-gollo <massimo.gollo@hotmail.it>
2022-05-05 18:58:01 +0200

    add ns monitoring in helm template
    - removed from script

--- helpers/starter/deploy_kps.sh
+++ helpers/starter/deploy_kps.sh
@@ -2,7 +2,8 @@
 set -eux
 BASEPATH=$(pwd)
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts && helm repo update
-kubectl create ns monitoring
+##TODO moved in helm chart vp-cloud
+#kubectl create ns monitoring
 #expose grafana
 helm install kps-cloud prometheus-community/kube-prometheus-stack -n monitoring -f $BASEPATH/custom-kps/values.kps.yaml --set grafana.service.type=NodePort
 kubectl patch service kps-cloud-grafana -n monitoring --type='json' --patch='[{"op": "replace", "path": "/spec/ports/0/nodePort", "value":30002}]'
